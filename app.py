91503b9ad6bd70507026af0c0ab9e1a4949ac314
Igor Olszewski <olszewskiigor2000@gmail.com>
2022-10-13 21:31:24 +0200

    Simple login added to FlaskAPI

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import json
 import os
+import sys
+import smtplib
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
@@ -6,14 +9,13 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
-import sys
 from collections import Counter, defaultdict
 from itertools import groupby
 from operator import itemgetter
 from timeit import timeit
-import smtplib
 
-# What is this?
+
+# hmm
 lista = [0]
 server = smtplib.SMTP('smtp.gmail.com', 587)
 
@@ -28,6 +30,7 @@ Session(app)
 if __name__ == '__main__':
     app.run(debug=True)
 
+
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -35,16 +38,21 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 # Database
 db = SQL("sqlite:///database.db")
 
 # Routes
 # Index Page Json
+
+
 @app.route('/api/index')
 def index():
-    return jsonify(komunikat="hejka",czy_dziala="tak")
+    return jsonify(komunikat="hejka", czy_dziala="tak")
 
 # Authors Page Json
+
+
 @app.route('/api/authors')
 def authors():
     # Define Variable
@@ -66,17 +74,16 @@ def authors():
         "nazwisko": nazwisko2,
         "id": Itemid2
     }
-    return jsonify(value,value2)
+    return jsonify(value, value2)
 
 # Register Page Json
+
+
 @app.route("/api/register", methods=["GET", "POST"])
 def register():
-    # Render register page json
     if request.method == "GET":
         info = 'Strona rejestracji'
         return jsonify(info)
-
-    # Checking if user provided all the required fields correctly and if the username is taken
     elif request.method == "POST":
         if not request.args.get("username"):
             return jsonify("You must provide a username.")
@@ -95,21 +102,41 @@ def register():
             rows = db.execute("SELECT * FROM users WHERE username = :username",
                               username=request.args.get("username"))
             session["user_id"] = rows[0]["user_id"]
-            return redirect("/")
+            return redirect("/api/index")
         elif True:
             return jsonify("Username was already taken!")
-### sample subpages below with api
 
-#@app.route('/api/hello')
-#def hello():
+
+@app.route("/api/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        info = 'Strona logowania'
+        return jsonify(info)
+
+    if request.method == "POST":
+        if not request.args.get("username"):
+            return jsonify("must provide username", 403)
+        elif not request.args.get("password"):
+            return jsonify("must provide password", 403)
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.args.get("username"))
+        if len(rows) != 1 or not check_password_hash(rows[0]["hashword"], request.args.get("password")):
+            return jsonify("invalid username and/or password", 403)
+        session["user_id"] = rows[0]["user_id"]
+        return jsonify("Zalogowano")
+
+# sample subpages below with api
+
+# @app.route('/api/hello')
+# def hello():
 #    return jsonify(simanko="no_hej")
 
-#@app.route('/api/user/<username>')
-#def show_user(username):
+# @app.route('/api/user/<username>')
+# def show_user(username):
 #    # returns the username
 #    return 'Username: %s' % username
 
-#@app.route('/api/post/<int:post_id>')
-#def show_post(post_id):
+# @app.route('/api/post/<int:post_id>')
+# def show_post(post_id):
     # returns the post, the post_id should be an int
 #   return str(post_id)
