7104295591e99bb4efa4f0000aec5b715a28b4a5
Dawid Bobowski <dbobowski3@gmail.com>
2022-11-29 21:42:23 +0100

    Avatar implementation adjustment

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,28 +1,41 @@
+import { useState } from 'react';
 import { Grid } from '@mui/material';
 import { useAppSelector } from '../../app/hooks';
 import { PageTitle } from '../common';
 import { IconButton } from '@mui/material';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
-import { ChangeEvent, useState } from 'react';
 import Avatar from '@mui/material/Avatar';
-import Photo from '../../assets/static/user.jpg'
+
 function Dashboard(): JSX.Element {
-  const username: string | null = useAppSelector(
-    (state) => state.user.username
-  );
+  const username: string | null = useAppSelector((state) => state.user.username);
+  const [importedPicture, setImportedPicure] = useState<File>();
+
   return (
-    <Grid container component='main' id='dashboard-container' sx={styles.grid}>
+    <Grid
+      container
+      component='main'
+      id='dashboard-container'
+      sx={styles.grid}
+    >
       <Avatar
-        src={Photo}
+        src={importedPicture ? URL.createObjectURL(importedPicture) : `src/assets/users/${username}.jpg`}
         sx={{ width: 70, height: 70 }}
       />
       <PageTitle title='Dashboard' />
       <span>Cześć, {username}!</span>
       <span>Wypełnij test osobowości oraz test zainteresowań i sprawdź swoje aktualne dopasowanie.</span>
       <span>Koniecznie wgraj lub zaktualizuj swoje zdjęcie ;)</span>
-      <IconButton aria-label="upload picture" component="label">
-      <input hidden accept="image/*" type="file"/>
-      <PhotoCamera />
+      <IconButton
+        aria-label='upload picture'
+        component='label'
+      >
+        <input
+          hidden
+          accept='image/*'
+          type='file'
+          onChange={(event) => event.target.files && setImportedPicure(event.target.files[0])}
+        />
+        <PhotoCamera />
       </IconButton>
     </Grid>
   );
