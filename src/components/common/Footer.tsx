e79be2e3b975050e7e939796837ce249839678fc
kingasmi <kinga.smialek@interia.pl>
2022-12-08 15:46:11 +0100

    footer
    sprawdzanie innych kolorów

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ function Footer(): JSX.Element {
   return (
     <footer id='page-footer'>
       
-      <h3>Dawid Bobowski,Igor Olszewski, Edyta Górka, Kinga Śmiałek, Karolina Wiśniewska <i>Souldate &copy; 2022</i></h3>
+      <h3> Zespół <i>Souldate &copy; 2022</i></h3>
     </footer>
   );
 }
