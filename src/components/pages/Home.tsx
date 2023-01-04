c64ec4877c3a4c59c88a6160573a4b4e110fa797
Dawid Bobowski <dbobowski3@gmail.com>
2023-01-04 22:05:40 +0100

    Home page UI rework v2

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,12 @@
 import { Avatar, Box, Button, Grid, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-import MainImage from '../../assets/jonathan-borba-couple2.jpg';
+import MainImage from '../../assets/sparks.jpg';
+import Heart from '../../assets/heart.png';
+import Dev1 from '../../assets/authors/dev1.png';
+import Dev2 from '../../assets/authors/dev2.png';
+import Dev3 from '../../assets/authors/dev3.png';
+import Dev4 from '../../assets/authors/dev4.png';
+import Dev5 from '../../assets/authors/dev5.png';
 
 // desktop UI to be reworked
 function Home(): JSX.Element {
@@ -10,10 +16,15 @@ function Home(): JSX.Element {
       component='main'
       id='home-container'
       sx={{
-        minHeight: '100vh',
         color: 'common.white',
         backgroundColor: 'rgb(224,159,62)',
         background: 'radial-gradient(circle, rgba(224,159,62,1) 0%, rgba(158,42,43,1) 100%)',
+        minHeight: '100vh',
+        width: '100%',
+        maxWidth: 1920,
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
       <Grid
@@ -22,108 +33,100 @@ function Home(): JSX.Element {
         sx={{
           display: 'flex',
           justifyContent: 'center',
+          overflow: 'hidden',
+          width: '100%',
+          height: '100vh',
         }}
       >
-        <Grid
-          item
-          sm={1}
-        />
-        <Grid
-          item
-          sm={8}
-        >
-          <Box
-            component='img'
-            src={MainImage}
-            sx={{
-              width: { sm: 1400 },
-              height: '100vh',
-              overflow: 'hidden',
-            }}
-          />
-        </Grid>
-        <Grid
-          item
-          sm={1}
-        />
-        <Box sx={{ position: 'absolute', top: { xs: 150, sm: 300 }, left: { xs: 25, sm: 200 } }}>
-          <Typography
-            variant='h1'
-            sx={{
-              fontSize: { xs: '3rem', sm: '6rem' },
-              fontFamily: '"Yeseva One", cursive',
-              textShadow: '5px 5px rgba(18, 18, 18, 1)',
-            }}
-          >
-            Twoja bratnia dusza
-          </Typography>
-          <Typography
-            variant='h1'
-            sx={{
-              position: 'relative',
-              left: 50,
-              fontSize: { xs: '1.75rem', sm: '3.5rem' },
-              maxWidth: '80%',
-              fontFamily: '"Yeseva One", cursive',
-              textShadow: '3px 3px rgba(18, 18, 18, 1)',
-            }}
-          >
-            Na wyciągnięcie ręki
-          </Typography>
-        </Box>
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: { xs: 'column', sm: 'row' },
-            gap: '1rem',
-            position: 'absolute',
-            top: { sm: 550 },
-            right: { sm: 200 },
-            bottom: { xs: 100, sm: 'unset' },
-            margin: { xs: '0 auto', sm: 'unset' },
+            width: '100%',
+            background: `url(${MainImage})`,
+            backgroundPosition: 'center',
+            position: 'relative',
           }}
         >
-          <Button
-            to='/login'
-            aria-label='login'
-            variant='contained'
-            component={Link}
+          <Box
+            id='main-page-slogan'
+            sx={{ position: 'absolute', top: 600, left: { xs: 30, sm: 100 } }}
+          >
+            <Typography
+              variant='h1'
+              sx={{
+                fontSize: { xs: '3rem', md: '4.5rem', lg: '6rem' },
+                fontFamily: '"Yeseva One", cursive',
+                textShadow: '5px 5px rgba(18, 18, 18, 1)',
+              }}
+            >
+              Twoja bratnia dusza
+            </Typography>
+            <Typography
+              variant='h1'
+              sx={{
+                position: 'relative',
+                left: 50,
+                fontSize: { xs: '1.75rem', md: '2.25rem', lg: '3.5rem' },
+                fontFamily: '"Yeseva One", cursive',
+                textShadow: '3px 3px rgba(18, 18, 18, 1)',
+              }}
+            >
+              Na wyciągnięcie ręki
+            </Typography>
+          </Box>
+          <Box
+            id='main-page-buttons'
             sx={{
-              background: 'linear-gradient(#6d1416 0%, #B24C32 30%, #B24C32 70%, #6d1416 100%)',
-              color: 'common.white',
-              width: '12rem',
-              fontSize: '1.5rem',
-              fontFamily: 'Poppins, sans-serif',
-              textShadow: '2px 2px rgba(18, 18, 18, 1)',
-              textTransform: 'capitalize',
-              ':hover': {
+              display: 'flex',
+              flexDirection: 'row',
+              gap: '1rem',
+              position: 'absolute',
+              bottom: 50,
+              right: { xs: 'unset', sm: 200 },
+              left: { xs: 'calc((100% - 25rem) / 2)', lg: 'unset' },
+            }}
+          >
+            <Button
+              to='/login'
+              aria-label='login'
+              variant='contained'
+              component={Link}
+              sx={{
+                background: 'linear-gradient(#6d1416 0%, #B24C32 30%, #B24C32 70%, #6d1416 100%)',
                 color: 'common.white',
-                background: 'linear-gradient(#500d0e 0%, #8a2022 30%, #8a2022 70%, #500d0e 100%)',
-              },
-            }}
-          >
-            Logowanie
-          </Button>
-          <Button
-            to='/register'
-            aria-label='register'
-            variant='contained'
-            component={Link}
-            sx={{
-              background: 'linear-gradient(#6d1416 0%, #B24C32 30%, #B24C32 70%, #6d1416 100%)',
-              color: 'common.white',
-              width: '12rem',
-              fontSize: '1.5rem',
-              fontFamily: 'Poppins, sans-serif',
-              textTransform: 'capitalize',
-              textShadow: '2px 2px rgba(18, 18, 18, 1)',
-              ':hover': {
-                background: 'linear-gradient(#500d0e 0%, #8a2022 30%, #8a2022 70%, #500d0e 100%)',
-              },
-            }}
-          >
-            Rejestracja
-          </Button>
+                width: '12rem',
+                fontSize: '1.5rem',
+                fontFamily: 'Poppins, sans-serif',
+                textShadow: '2px 2px rgba(18, 18, 18, 1)',
+                textTransform: 'capitalize',
+                ':hover': {
+                  color: 'common.white',
+                  background: 'linear-gradient(#500d0e 0%, #8a2022 30%, #8a2022 70%, #500d0e 100%)',
+                },
+              }}
+            >
+              Logowanie
+            </Button>
+            <Button
+              to='/register'
+              aria-label='register'
+              variant='contained'
+              component={Link}
+              sx={{
+                background: 'linear-gradient(#6d1416 0%, #B24C32 30%, #B24C32 70%, #6d1416 100%)',
+                color: 'common.white',
+                width: '12rem',
+                fontSize: '1.5rem',
+                fontFamily: 'Poppins, sans-serif',
+                textTransform: 'capitalize',
+                textShadow: '2px 2px rgba(18, 18, 18, 1)',
+                ':hover': {
+                  background: 'linear-gradient(#500d0e 0%, #8a2022 30%, #8a2022 70%, #500d0e 100%)',
+                },
+              }}
+            >
+              Rejestracja
+            </Button>
+          </Box>
         </Box>
       </Grid>
       <Grid
@@ -131,41 +134,47 @@ function Home(): JSX.Element {
         sx={{
           display: 'flex',
           justifyContent: 'center',
-          padding: '5rem 0',
         }}
       >
         <Grid
           item
-          width={1000}
           sx={{
             color: 'common.black',
             backgroundColor: 'common.white',
+            width: '100%',
+            boxShadow: '0px 0px 15px -5px rgba(10, 10, 10, 1)',
             position: 'relative',
-            left: 100,
+            marginTop: '5rem',
           }}
         >
           <Typography
             variant='h5'
-            sx={{ maxWidth: 1000 }}
+            sx={{ padding: { xs: '5rem', md: '5rem 10rem' }, maxWidth: 1000, margin: '0 auto' }}
           >
-            Jeśli szukasz bratniej duszy, z którą będziesz mógł dzielić swoje pasje, marzenia i emocje, to SoulDate jest
-            dla Ciebie. Nasz algorytm znajdzie osobę najbardziej podobną do Ciebie na podstawie wspólnych zainteresowań
-            i preferencji, dzięki czemu masz pewność, że trafisz na osobę, która naprawdę Cię rozumie. Nie czekaj,
-            dołącz już dziś i znajdź swoją bratnią duszę!
+            Jeśli szukasz bratniej duszy, z którą chciał_byś dzielić swoje pasje, marzenia i emocje, to <b>SoulDate</b>{' '}
+            jest dla Ciebie. Nasz algorytm znajdzie osobę najbardziej podobną do Ciebie na podstawie wspólnych
+            zainteresowań i preferencji, dzięki czemu masz pewność, że trafisz na osobę, która naprawdę Cię rozumie.{' '}
+            <b>Nie czekaj</b>, dołącz już dziś i znajdź swoją bratnią duszę!
+            <Box
+              component='img'
+              src={Heart}
+              width={300}
+              sx={{ float: 'right', margin: { xs: '3rem -2rem 5rem 0', md: '3rem -6rem 5rem 0' } }}
+            />
           </Typography>
         </Grid>
         <Grid
           item
-          width={1000}
           sx={{
             color: 'common.black',
             backgroundColor: 'common.white',
-            position: 'relative',
-            left: 100,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            paddingTop: '5rem',
+            marginTop: '5rem',
+            width: '100%',
+            padding: { xs: '5rem', sm: '5rem 10rem' },
+            boxShadow: '0px 0px 15px -5px rgba(10, 10, 10, 1)',
           }}
         >
           <Typography variant='h2'>Nasz zespół</Typography>
@@ -181,7 +190,7 @@ function Home(): JSX.Element {
             <Box sx={styles.teamBox}>
               <Avatar
                 alt='członek zespołu 1'
-                src=''
+                src={Dev1}
                 sx={styles.teamAvatar}
               />
               <Typography
@@ -193,8 +202,21 @@ function Home(): JSX.Element {
             </Box>
             <Box sx={styles.teamBox}>
               <Avatar
+                alt='członek zespołu 4'
+                src={Dev4}
+                sx={styles.teamAvatar}
+              />
+              <Typography
+                variant='caption'
+                sx={styles.teamTypography}
+              >
+                Edyta Górka
+              </Typography>
+            </Box>
+            <Box sx={styles.teamBox}>
+              <Avatar
                 alt='członek zespołu 2'
-                src=''
+                src={Dev2}
                 sx={styles.teamAvatar}
               />
               <Typography
@@ -207,7 +229,7 @@ function Home(): JSX.Element {
             <Box sx={styles.teamBox}>
               <Avatar
                 alt='członek zespołu 3'
-                src=''
+                src={Dev3}
                 sx={styles.teamAvatar}
               />
               <Typography
@@ -219,21 +241,8 @@ function Home(): JSX.Element {
             </Box>
             <Box sx={styles.teamBox}>
               <Avatar
-                alt='członek zespołu 4'
-                src=''
-                sx={styles.teamAvatar}
-              />
-              <Typography
-                variant='caption'
-                sx={styles.teamTypography}
-              >
-                Edyta Górka
-              </Typography>
-            </Box>
-            <Box sx={styles.teamBox}>
-              <Avatar
                 alt='członek zespołu 5'
-                src=''
+                src={Dev5}
                 sx={styles.teamAvatar}
               />
               <Typography
@@ -245,6 +254,14 @@ function Home(): JSX.Element {
             </Box>
           </Grid>
         </Grid>
+        <Box sx={{ margin: '4rem 0', width: '100%', textAlign: 'center' }}>
+          <Typography
+            variant='h3'
+            sx={{ fontSize: '1.5rem', fontFamily: 'Poppins, sans-serif' }}
+          >
+            SoulDate &copy; 2023
+          </Typography>
+        </Box>
       </Grid>
     </Grid>
   );
@@ -264,5 +281,6 @@ const styles = {
   },
   teamTypography: {
     marginTop: '1rem',
+    fontSize: { xs: '1.25rem', sm: 'inherit' },
   },
 };
